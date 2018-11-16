a3a3d267eb43f1be6d7a05c9d201b5897c37b4fe
hecaiont <dlsxo1518@gmail.com>
2018-11-16 22:34:04 +0900

    Changed index.html -sm- to -xs- for mobile view and setting allowedhost added.

--- recipe_calculator/settings.py
+++ recipe_calculator/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'u5+6c+8+2&*ej82@4@ex9b7)#z#!w1iax^^esx8e+b6b7ky891'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 
 # Application definition
