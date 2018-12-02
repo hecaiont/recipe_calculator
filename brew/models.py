26c105247c8bfe7baf328bf5af1b867b025cebf2
hecaiont <dlsxo1518@gmail.com>
2018-12-02 20:19:35 +0900

    brew model & admin cho regist

--- brew/models.py
+++ brew/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class recipe(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    details = models.TextField()
+    created_date = models.DateTimeField(
+            default=timezone.now)
+    # brewing_date = models.DateTimeField(
+    #         blank=True, null=True)
+
+    def stage(self):
+        rice = models.IntegerField()
+        water = models.IntegerField()
+        yeast = models.IntegerField()
+        flour = models.IntegerField()
+        sub_ingredient = models.IntegerField()
+        term = models.IntegerField()
