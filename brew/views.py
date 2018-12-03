2046ce03d54880c510f3b6c008dd4be522f4098a
hecaiont <dlsxo1518@gmail.com>
2018-12-03 19:55:25 +0900

    django dynamic template begin

--- brew/views.py
+++ brew/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
-from django.template import loader
+# from django.template import loader
+from django.utils import timezone
+from .models import recipe
 
 
-def index(request):
-    template = loader.get_template('brew/index.html')
-    context = {
-        'latest_question_list': "test",
-    }
-    return HttpResponse(template.render(context, request))
+# def index(request):
+#     template = loader.get_template('brew/index.html')
+#     context = {
+#         'latest_question_list': "test",
+#     }
+#     return HttpResponse(template.render(context, request))
+
+
+
+def recipe_list(request):
+    recipes = recipe.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+
+    return render(request, 'brew/recipe_list.html', {'recipes': recipes})
+
