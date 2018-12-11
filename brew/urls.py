f549c4e14c8e787ce8df88da47adc392da0178a8
hecaiont <dlsxo1518@gmail.com>
2018-12-11 22:42:44 +0900

    recipe detail fail revert backing up

--- brew/urls.py
+++ brew/urls.py
@@ -1,7 +1,9 @@
 from django.urls import url, include
 from django.contrib import admin
+from .import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^brew/', include('brew.urls')),
+    # url(r'^recipe/(?P<pk>\d+)/$', views.recipe_detail, name='recipe_detail'),
 ]
--- brew/views.py
+++ brew/views.py
@@ -1,10 +1,10 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 # from django.http import HttpResponse
 
 # from django.template import loader
 from django.utils import timezone
 from .models import recipe
-
+from django.shortcuts import render, get_object_or_404
 
 # def index(request):
 #     template = loader.get_template('brew/index.html')
@@ -14,9 +14,11 @@ from .models import recipe
 #     return HttpResponse(template.render(context, request))
 
 
-
 def recipe_list(request):
     recipes = recipe.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
-
     return render(request, 'brew/recipe_list.html', {'recipes': recipes})
 
+
+# def recipe_detail(request, pk):
+#     detail_view = get_object_or_404(recipe, pk=pk)
+#     return render(request, 'brew/recipe_detail.html', {'detail_view': detail_view})
